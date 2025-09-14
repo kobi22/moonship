8e9323cf5e51d76e46e023f0019d8d0b9a74b3e7
kobi22 <yacovifields40@gmail.com>
2025-09-14 09:40:58 -0500

    Update build configs for Tailwind + Vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginTailwind()],
-  define: {
-    global: "globalThis", // Fix Buffer issue
-  },
-  optimizeDeps: {
-    include: ["buffer"],
-  },
+  plugins: [react()],
 });
