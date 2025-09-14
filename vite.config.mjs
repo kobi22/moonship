c9727752f37a57c43ad2b51407c00306ecbc55bf
kobi22 <56238139+kobi22@users.noreply.github.com>
2025-09-14 17:49:33 -0500

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vitePluginTailwind from "vite-plugin-tailwindcss";
 
 export default defineConfig({
   plugins: [react(), vitePluginTailwind()],
